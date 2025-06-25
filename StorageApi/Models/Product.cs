fdf1b7f6cce29ea6532f5b0cdf1d062fbc8f2a85
a21jeaha <hisui86@gmail.com>
2025-06-25 11:12:10 +0200

    initial commit

---
+++ StorageApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Product
+{
+	public  int Id { get; set; }
+	public string Name { get; set; }
+	public int Price { get; set; }
+	public string Category { get; set; }
+	public string Shelf { get; set; }
+	public int Count { get; set; }
+	public string Discription { get; set; }
+}
